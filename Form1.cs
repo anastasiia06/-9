46a13434a8ebff5837081aa38e93aac35ebbab79
anastasiia06 <anastasiia.lily06@gmail.com>
2025-06-05 12:32:36 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-namespace WinFormsApp9
+namespace WinFormsApp8
 {
     public partial class Form1 : Form
     {
@@ -11,60 +11,30 @@ namespace WinFormsApp9
         {
 
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
+            int n;
+            dataGridView1.Rows.Clear();
             Random ran = new Random();
-            int n, m;
+
             try
             {
                 n = Convert.ToInt32(textBox1.Text);
-                m = Convert.ToInt32(textBox2.Text);
+                Arrays a = new Arrays(n); 
 
-                DataGridViewTextBoxColumn dgvAge;
-                for (int i = 0; i < m; i++)
+                for (int i = 0; i < a.Length; i++)
                 {
-                    dgvAge = new DataGridViewTextBoxColumn();
-                    dgvAge.Width = 40;
-                    dataGridView1.Columns.Add(dgvAge);
+                    a[i] = ran.Next(-100, 100);
+                    dataGridView1.Rows.Add(a[i].ToString());
                 }
 
-                dataGridView1.Rows.Clear();
-                dataGridView1.ColumnCount = m + 1;
-                dataGridView1.RowCount = n;
-
-                TwoArray a = new TwoArray(n, m + 1);
-                for (int i = 0; i < n; i++)
-                {
-                    a[i, 0] = i + 1;
-                    dataGridView1.Rows[i].Cells[0].Value = a[i, 0].ToString();
-                }
-
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = 1; j <= m; j++)
-                    {
-                        a[i, j] = ran.Next(0, 100);
-                        dataGridView1.Rows[i].Cells[j].Value = a[i, j].ToString();
-                    }
-                }
-
-                if (a.error)
-                {
-                    MessageBox.Show("Помилка");
-                }
-
-                int zeh = a.Min[0] + 1;
-                label3.Text = "Цех з мінімальною кількістю ресурсів: " + zeh.ToString();
-
-                int[] massiv = a.Min;
-                dataGridView1.Rows[a.Min[0]].DefaultCellStyle.BackColor = Color.Yellow;
+                if (a.error) MessageBox.Show("Помилка обробки даних");
+                label2.Text = "Мінімальний елемент масива: " + a.Min.ToString();
             }
             catch
             {
-                MessageBox.Show("Помилка");
+                MessageBox.Show("Помилка введення даних");
             }
         }
     }
-    
 }
